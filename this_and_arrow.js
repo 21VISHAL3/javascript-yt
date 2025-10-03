f08fdbc47aa1710e0be424665261375e28c2f9b6
21vishal3 <vishal98017kmr@gmail.com>
2025-10-03 17:30:43 +0530

    this arrow func commit

--- this_and_arrow.js
+++ this_and_arrow.js
@@ -1,35 +1,37 @@
-const user = {
-    username: "ram",
-    price: 999,        // this. current context ko refer krta h
+// `const user = {
+//     username: "ram",
+//     price: 999,        // this. current context ko refer krta h
 
-    welcomeMessage: function() {
-       console.log(`${this.username} , welcome to website`)
-       console.log(this) //current context
-    }
-}
+//     welcomeMessage: function() {
+//        console.log(`${this.username} , welcome to website`)
+//        console.log(this) //current context
+//     }
+// }`
 
-//user.welcomeMessage()
+// user.welcomeMessage()
 //user.username = "shyam"
 //user.welcomeMessage()
-//console.log(this)  // refer empty object bcs global k under koi object hi nhi h 
+// console.log(this)  // refer empty object bcs global k under koi object hi nhi h 
 // browser k under jo pbject h oo global scope h
 
-/*function chai()
-{
-    let username = "ram"          
-    console.log(this.username);  // not working inside function
-}
-chai()
-*/
+// function chai()
+// {
+//     let username = "ram"          
+//     console.log(this.username);  // not working inside function
+// }
+// chai()
 
-/*const chai = () => {       //**arrow function
+
+const chai = () => {       //**arrow function
     let username = "ram"
     console.log(this.username);
 }
 chai()
-*/
+
 
 const addTwo = (num1, num2) => {
-    return num1 + num2;
+    return num1 + num2; // EXPLICIT RETURN
+    //const addTwo = (num1, num2) => (num1 + num2) *IMPLICIT RETURN*
+    // const addTwo = (num1, num2) => ({username: "hitesh"})
     console.log(addTwo(3,4))
 }
